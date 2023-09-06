847e7f183eba931e9c187a6192033c0b39a0717d
Valed7x <barnoucis@gmail.com>
2023-09-05 17:16:12 -0700

    When I was having that alphabet soup

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-'''that prints the ASCII alphabet, in lowercase, not followed by a new line.'''
-
 for i in range(ord('a'), ord('z') + 1):
     if i != ord('e') and i != ord('q'):
         print('{}'.format(chr(i)), end='')
