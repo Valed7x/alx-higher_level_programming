0ca0209bdd39eafd7fc692fb8f5a1f4169622cbc
Valed7x <barnoucis@gmail.com>
2023-09-05 17:14:17 -0700

    When I was having that alphabet soup

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 '''that prints the ASCII alphabet, in lowercase, not followed by a new line.'''
 
 for letter in range(97, 123):
-    if letter != 101 and letter != 113:
-        print("{}".format(chr(letter)), end='')
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
