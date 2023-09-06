87518f91e7663194decc12f101a99531b1a11529
Valed7x <barnoucis@gmail.com>
2023-09-05 19:01:36 -0700

    ByteCode -> Python #2

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Python function def magic_calculation(a, b, c)'''
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    return a * b - c
