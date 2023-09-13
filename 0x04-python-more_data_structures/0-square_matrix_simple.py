e31bdba8e7fb8700410f610bca6a9eca6340ba2d
Valed7x <barnoucis@gmail.com>
2023-09-13 16:41:39 -0700

    Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        raw = []
+        for j in i:
+            j = j * j
+            raw.append(j)
+        new_matrix.append(raw)
+    return new_matrix
