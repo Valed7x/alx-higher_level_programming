9ffc0e7fbd8d4465814e1b3afedf97ca6d3e5c49
Valed7x <barnoucis@gmail.com>
2023-09-13 16:43:41 -0700

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    '''function that replaces all occurrences of an element by another in a new list.'''
+    copy = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            copy.append(replace)
+        else:
+            copy.append(my_list[i])
+    return copy
