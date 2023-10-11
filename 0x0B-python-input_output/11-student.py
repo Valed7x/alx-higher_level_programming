8e28cdfa832c1524f72439f71b20856f0b511e5f
Valed7x <barnoucis@gmail.com>
2023-10-11 00:40:47 +0000

    Student to disk and reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,19 +1,46 @@
 #!/usr/bin/python3
 """
-importing a class
-from the parent.
+Defines a class Student.
 """
-Rectangle = __import__("9-rectangle").Rectangle
 
 
-"""A Square class that inherits from a Rectangle class"""
+class Student:
+    """Represent a student."""
 
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
 
-class Square(Rectangle):
-    """A init function with the size"""
+        Args:
+            first_name (str): The first name of the student.
 
-    def __init__(self, size):
-        self.integer_validator("size", size)
+            last_name (str): The last name of the student.
 
-        super().__init__(size, size)
-        self.__size = size
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+
+        If attrs is a list of strings, represents only those attributes
+
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
