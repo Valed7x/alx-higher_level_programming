8c1e94a49a7fab88b21971f4d71611c4e8aac18f
Valed7x <barnoucis@gmail.com>
2023-10-11 00:36:35 +0000

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+importing a class
+
+from the parent.
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+"""A Square class that inherits from a Rectangle class"""
+
+
+class Square(Rectangle):
+    """A init function with the size"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)
+        self.__size = size
